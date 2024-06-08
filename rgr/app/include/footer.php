d5deed5bb277b5bafaae2d0c6a8d618ad5daf6d7
sssnnntt <152970930+sssnnntt@users.noreply.github.com>
2024-06-08 10:01:10 +0700

    Add files via upload

---
+++ rgr/app/include/footer.php
@@ -0,0 +1,21 @@
+<div class="footer container-fluid">
+    <div class="footer-content container-fluid">
+        <div class="row">
+            <div class="footer-section about col-12">
+                <h3 class="logo-text col-12">AutoClub</h3>
+
+                <div class="contact col-12">
+                    <span><i class="fas fa-phone"></i> &nbsp; 123-456-789</span>
+                    <span><i class="fas fa-envelope"></i> &nbsp; info@autoclub.com</span>
+                </div>
+                <div class="socials col-12">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ rgr/path.php
@@ -0,0 +1,17 @@
+<?php
+// path.php
+
+if (!defined('SITE_ROOT')) {
+    define('SITE_ROOT', __DIR__);
+}
+
+if (!defined('BASE_URL')) {
+    define('BASE_URL', "http://localhost/rgr/");
+}
+
+if (!defined('ROOT_PATH')) {
+    define('ROOT_PATH', realpath(dirname(__FILE__)));
+}
+?>
+
+
